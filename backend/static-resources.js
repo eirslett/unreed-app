6755bb3e7a8038e817e3e9e582dd6d3d36e7fbf0
Eirik Sletteberg <eiriksletteberg@gmail.com>
2024-05-19 02:46:23 +0200

    Fix 404 on reload page

--- backend/static-resources.js
+++ backend/static-resources.js
@@ -38,6 +38,6 @@ export async function setupIndexHtml(app) {
     }
     app.use(serveIndexHtml);
   } else {
-    app.use((req, res) => res.sendFile(path.resolve(__dirname, 'dist/index.html')));
+    app.use((req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html')));
   }
 }
